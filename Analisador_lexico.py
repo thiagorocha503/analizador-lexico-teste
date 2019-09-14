e1068d74c2f6cdcdd1c28962f2c61a39f7a4cb79
ThiagoRocha <thiagorocha503@gmail.com>
2019-09-14 11:59:59 -0300

    Refatoração do código

--- Analisador_lexico.py
+++ Analisador_lexico.py
@@ -1,204 +1,195 @@
-#-*-coding:utf8;-*-
-#qpy:3
-#qpy:console
 
-
-
-alfabeto=("a","b","c","d","e",
-"f","g","h","i","j","k","l","m",
-"n","o","p","q","r","s","t","u",
-"v","w","x","y","z","A","B","C",
-"D","E","F","G","H","I","J","K",
-"L","M","N","O","P","Q","R","S",
-"T","U","V","W","X","Y","Z","Á",
-"À","Ã","Â","É","Ê","Í","Ó","Ô",
-"Õ","Ú","á","à","â","ã","é","ê",
-"í","ó","ô","õ","ú","ç","ñ","Ñ",
-"_")
-numeros = ("0","1","2","3","4","5","6","7","8","9")#,".")          
-operadores = ("=","+","-","/","*","%","<",">","!")
-outros = (",",":",";","#",".")
-delimitadores =("(",")","{","}","[","]")
-reservadas =("while","for","break",
-"continue","case","if","else","int",
-"float","str")
-marcadores = ("\n","\t")
+alfabeto = ("a", "b", "c", "d", "e", "f", "g", "h",
+            "i", "j", "k", "l", "m", "n", "o", "p",
+            "q", "r", "s", "t", "u", "v", "w", "x",
+            "y", "z", "A", "B", "C", "D", "E", "F",
+            "G", "H", "I", "J", "K", "L", "M", "N",
+            "O", "P", "Q", "R", "S", "T", "U", "V",
+            "W", "X", "Y", "Z", "Á", "À", "Ã", "Â",
+            "É", "Ê", "Í", "Ó", "Ô", "Õ", "Ú", "á",
+            "à", "â", "ã", "é", "ê", "í", "ó", "ô",
+            "õ", "ú", "ç", "ñ", "Ñ", "_")
+numeros = ("0", "1", "2", "3", "4", "5", "6", "7", "8", "9")  # ,".")
+operadores = ("=", "+", "-", "/", "*", "%", "<", ">", "!")
+outros = (",", ":", ";", "#", ".")
+delimitadores = ("(", ")", "{", "}", "[", "]")
+reservadas = ("while", "for", "break", "swith",
+              "continue", "case", "if", "else", "int",
+              "float", "str")
+marcadores = ("\n", "\t")
 # Caracter válidos para strings
-validos = ("0","1","2","3","4","5","6",
-"7","8","9","a","b","c","d","e","f",
-"g","h","i","j","k","l","m","n","o",
-"p","q","r","s","t","u","v","w","x",
-"y","z","A","B","C","D","E","F","G",
-"H","I","J","K","L","M","N","O","P",
-"Q","R","S","T","U","V","W","X","Y",
-"Z","Á","À","Ã","Â","É","Ê","Í","Ó",
-"Ô","Õ","Ú","á","à","â","ã","é","ê","í",
-"ó","ô","õ","ú","ñ","Ñ","ç","{","}","[",
-"]","/","(",")",".","=","+","-","*",
-"%","<",">","!","'",":",";"," ","_", #Aspa não é incluída
-",","?","`","~","¿","¡","^","!","$",
-"#","@","×","÷","€","£","¥","₩","☆",
-"♤","♡","◇","♧","°","\t")
+caracterValidosString = numeros + alfabeto + delimitadores + outros + operadores
 
-#     NUMERAL 
+
+#     NUMERAL
 def numeral():
-    global i
+    global indice
     global erros
     ponto = 0
     num = ""
-    while i < tamanho:
-        if (codigo[i] in numeros) or (codigo[i] == "."):
-            if codigo[i] == ".": 
-                ponto += 1              
-                if ponto == 1:# Verifica se ponto é permitido
-                    num = num + "." # adiciona o ponto decimal 
-                    i += 1
+    while indice < tamanho:
+        if (codigo[indice] in numeros) or (codigo[indice] == "."):
+            if codigo[indice] == ".":
+                ponto += 1
+                if ponto == 1:  # Verifica se ponto é permitido
+                    num = num + "."  # adiciona o ponto decimal
+                    indice += 1
                 else:
-                    print("Linha %4.d - Erro 01: Ponto decimal a mais:"%linha)
-                    print("Numeral: ",num+codigo[i])
-                    print(" "*8," "*len(num),"^")# ponteiro de erro
+                    print("Linha %4.d - Erro 01: Ponto decimal a mais:" % linha)
+                    print("Numeral: ", num + codigo[indice])
+                    print(" " * 8, " " * len(num), "^")  # ponteiro de erro
                     erros += 1
-                    i += 1# ignora o ponto a mais
-                    break             
-            else: # Se não,  número.
-                num = num + codigo[i]         
-                i += 1
-        else:    
+                    indice += 1  # ignora o ponto a mais
+                    break
+            else:  # Se não,  número.
+                num = num + codigo[indice]
+                indice += 1
+        else:
             # Verifica se caracter é válido         
-            if codigo[i] in ('"'," ") or (codigo[i] in (operadores + outros + alfabeto + delimitadores)):             
-                break #  Fim do número
-            elif codigo[i] == "\n" or codigo[i] == "\t":
-                break    
-            else:# Caracter inválido
-                aux = num+codigo[i] # variável auxiliar 
-                print("Linha %4.d - Erro 02:"%linha)
-                print("Caracter inválido: ",aux)         	
-                print(" "*17," "*len(aux),"^")# ponteiro de erro
-                erros += 1 
-                i += 1 # pula o erro
-                break   
-            
-    if  ponto == 0:
+            if codigo[indice] in ('"', " ") or (codigo[indice] in (operadores + outros + alfabeto + delimitadores)):
+                break  # Fim do número
+            elif codigo[indice] == "\n" or codigo[indice] == "\t":
+                break
+            else:  # Caracter inválido
+                aux = num + codigo[indice]  # variável auxiliar
+                print("Linha %4.d - Erro 02:" % linha)
+                print("Caracter inválido: ", aux)
+                print(" " * 17, " " * len(aux), "^")  # ponteiro de erro
+                erros += 1
+                indice += 1  # pula o erro
+                break
+
+    if ponto == 0:
         return int(num)
     else:
-        return float(num) 
-#      VARIÁVEIS/PALAVRAS-RESERVADAS
+        return float(num)
+    #      VARIÁVEIS/PALAVRAS-RESERVADAS
+
+
 def constante():
-    global i
+    global indice
     global erros
     const = ""
     while True:
-        if i < tamanho:
-            if codigo[i] in (alfabeto + numeros):
-                const = const + codigo[i]
-                i += 1
+        if indice < tamanho:
+            if codigo[indice] in (alfabeto + numeros):
+                const = const + codigo[indice]
+                indice += 1
             else:
-                if codigo[i] == '"' or codigo[i] == ".":
+                if codigo[indice] == '"' or codigo[indice] == ".":
                     break
-                elif codigo[i] in (marcadores + delimitadores):
+                elif codigo[indice] in (marcadores + delimitadores):
                     break
-                elif codigo[i] in (outros + operadores):
+                elif codigo[indice] in (outros + operadores):
                     break
-                elif codigo[i] == " ":
-                    i += 1 # pula espaço
-                    break                   
-                else: # caracter inválido
-                    const = const + codigo[i]
-                    print("Linha %4.d - Erro 03: caracter invalido"%linha)
+                elif codigo[indice] == " ":
+                    indice += 1  # pula espaço
+                    break
+                else:  # caracter inválido
+                    const = const + codigo[indice]
+                    print("Linha %4.d - Erro 03: caracter invalido" % linha)
                     print(const)
-                    print(" "*(len(const)-1),"^")
+                    print(" " * (len(const) - 1), "^")
                     erros += 1
-                    i += 1                          
+                    indice += 1
         else:
             break
     return const
+
+
 #  Palavras reservadas
-def reservada(Id):#
-    if string in resevarda:
+def reservada(_id):
+    if _id in reservadas:
         return True
     else:
         return False
+
+
 #    OPERADORES
 def operador():
-    global i
+    global indice
     global erros
-    simbolo = codigo[i]   
-    i += 1 # 
-    if i < tamanho:# Se não chegou ao fim do codigo
-        if codigo[i] == " ":# Se for espaços, avance.
-            pula_espaços()
-        else:    
-            if i < tamanho:    
-                next_lexema = codigo[i]#  próximo lexema valido            
-                if simbolo == "!":# "!" Não pode está desacompanhado de "="
+    simbolo = codigo[indice]
+    indice += 1  #
+    if indice < tamanho:  # Se não chegou ao fim do codigo
+        if codigo[indice] == " ":  # Se for espaços, avance.
+            pula_espacos()
+        else:
+            if indice < tamanho:
+                next_lexema = codigo[indice]  # próximo lexema valido
+                if simbolo == "!":  # "!" Não pode está desacompanhado de "="
                     if next_lexema == "=":
                         simbolo = "!="
-                        i += 1
+                        indice += 1
                     else:
-                        aux = simbolo+next_lexema
-                        print("Linha %4.d - Erro 04: operador invalido: %s"%(linha,aux))
-                        print(" "*41,"^")
+                        aux = simbolo + next_lexema
+                        print("Linha %4.d - Erro 04: operador invalido: %s" % (linha, aux))
+                        print(" " * 41, "^")
                         erros += 1
-                else:  
-                    if next_lexema in operadores:              
-                        if simbolo in ("+","-","/","*") and next_lexema == "=":
+                else:
+                    if next_lexema in operadores:
+                        if simbolo in ("+", "-", "/", "*") and next_lexema == "=":
                             simbolo = simbolo + next_lexema
-                            i +=1
+                            indice += 1
                         elif simbolo == "=" and next_lexema == "=":
                             simbolo = "=="
-                            i += 1
-                        elif simbolo in (">","<") and next_lexema == "=":
+                            indice += 1
+                        elif simbolo in (">", "<") and next_lexema == "=":
                             simbolo = simbolo + next_lexema
-                            i += 1                  
+                            indice += 1
                         else:
-                            simbolo = simbolo + next_lexema  
-                            print("Linha %4.d - Erro 05: operador invalido: %s"%(linha,simbolo))                    
-                            print(" "*41,"^")
-                            i += 1
+                            simbolo = simbolo + next_lexema
+                            print("Linha %4.d - Erro 05: operador invalido: %s" % (linha, simbolo))
+                            print(" " * 41, "^")
+                            indice += 1
                             erros += 1
     return simbolo
+
+
 #
 #        STRING
 #
 def string():
-    global i
+    global indice
     global erros
-    string = '"'
-    i += 1#pula aspas iniciais
-    if i < tamanho:
+    _string = '"'  # underline para outercope
+    indice += 1  # pula aspas iniciais
+    if indice < tamanho:
         while True:
-            if codigo[i] in validos:
-                string = string + codigo[i] 
-                i += 1        
-            elif codigo[i] == '"':
-                i += 1 # pula aspa final
+            if codigo[indice] in caracterValidosString:
+                _string = _string + codigo[indice]
+                indice += 1
+            elif codigo[indice] == '"':
+                indice += 1  # pula aspa final
                 break
-            elif codigo[i] == "\n":
-                print("Linha %4.d - Erro 06: String com quebra de linha"%linha)
-                print(string)
-                print(" "*len(string),"^")
-                i += 1
+            elif codigo[indice] == "\n":
+                print("Linha %4.d - Erro 06: String com quebra de linha" % linha)
+                print(_string)
+                print(" " * len(_string), "^")
+                indice += 1
                 erros += 1
                 break
             else:
-                string = string + codigo[i]
-                print("Linha %4.d - Erro 07: Caracter inválido"%linha)
-                print(string)
-                print(" "*(len(string)-1),"^")
-                i += 1 # próximo carácter
-                erros += 1  
-            if i >= tamanho:
-                print("Linha %4.d - Erro 08: Aspas não fechada:"%linha)
-                print(string)
-                print(" "*(len(string)-1),"^")               
+                _string = _string + codigo[indice]
+                print("Linha %4.d - Erro 07: Caracter inválido" % linha)
+                print(_string)
+                print(" " * (len(_string) - 1), "^")
+                indice += 1  # próximo carácter
+                erros += 1
+            if indice >= tamanho:
+                print("Linha %4.d - Erro 08: Aspas não fechada:" % linha)
+                print(_string)
+                print(" " * (len(_string) - 1), "^")
                 erros += 1
                 break
-    else:#Caso fim da lista apos aspa inicial
-        print("Linha %4.d - Erro 09: aspas não fechada"%linha)
-        print(string)
-        print(" "*(len(string)-1),"^")
-    string = string + '"'    
-    return string
+    else:  # Caso fim da lista apos aspa inicial
+        print("Linha %4.d - Erro 09: aspas não fechada" % linha)
+        print(_string)
+        print(" " * (len(_string) - 1), "^")
+    _string = _string + '"'
+    return _string
+
+
 """
 def marcador():
     global i 
@@ -210,81 +201,86 @@ def marcador():
     i+=1
     return x
 """
+
+
 def next_token(n):
     global erros
-    global i
-    # Verifica  que tipo de token se trata
-    if n in numeros: # números inteiros/reais 
-        token = numeral()
-    elif n == '"':# cadeia de caracteres
-        token = string()
-    elif n in alfabeto: # Identificadores/ palavras chaves              
-        token = constante()
-    elif n in operadores: # operadores aritméticos/relacionais
-        token = operador()
-    elif n in outros: # Outros símbolos válidos
-        token = codigo[i]
-        i += 1
-    elif n in delimitadores:# Delimitadores
-        token = codigo[i]
-        i += 1 
+    global indice
+    # Verifica  que tipo de _token se trata
+    if n in numeros:  # números inteiros/reais
+        _token = numeral()
+    elif n == '"':  # cadeia de caracteres
+        _token = string()
+    elif n in alfabeto:  # Identificadores/ palavras chaves
+        _token = constante()
+    elif n in operadores:  # operadores aritméticos/relacionais
+        _token = operador()
+    elif n in outros:  # Outros símbolos válidos
+        _token = codigo[indice]
+        indice += 1
+    elif n in delimitadores:  # Delimitadores
+        _token = codigo[indice]
+        indice += 1
     elif n == "\n":
-        token = "《n》"
-        #print("marcador: quebrar de linha "
-        i += 1
+        _token = "《n》"
+        # print("marcador: quebrar de linha "
+        indice += 1
     elif n == "\t":
-        token = "《t》"
-        #print("marcador: tabulação ")
-        i += 1
+        _token = "《t》"
+        # print("marcador: tabulação ")
+        indice += 1
     else:
-        token = codigo[i]
-        print("linha %4.d - Erro 10: Carácter invalido: %s"%(linha,token))       
+        _token = codigo[indice]
+        print("linha %4.d - Erro 10: Carácter invalido: %s" % (linha, _token))
         erros += 1
-        i += 1
-    return token
+        indice += 1
+    return _token
 
-def pula_espaços():
-    global i
-    global space 
-    if i < tamanho:
-        while codigo[i] == " ":
+
+def pula_espacos():
+    global indice
+    global space
+    if indice < tamanho:
+        while codigo[indice] == " ":
             space += 1
-            i += 1
-            if i >= tamanho:
+            indice += 1
+            if indice >= tamanho:
                 break
     else:
-        print("Fim")   
+        print("Fim")
 
-#     Entrada por arquivo
-space = 0 # Qtd de espaços 
-analise =[] #Lista que armazena os tokens
-arquivo = open("/sdcard/qpython/codigo.txt")
+    #     Entrada por arquivo
+
+
+space = 0  # Quantidade de espaços total
+analise = []  # Lista que armazena os tokens
+arquivo = open("codigo.txt")
 line = arquivo.readline()
-total_erros = 0# Erros de todas as linha do código
-linha = 0 # linha do código
+total_erros = 0  # Erros de todas as linha do código
+linha = 0  # linha atual de analize do código
 print()
 while line != "":
     linha += 1
     codigo = list(line)
     tamanho = len(codigo)
-    erros = 0 # Erros de uma linha do código 
-    i = 0 # indice da lista
-    while i < tamanho:
-        if codigo[i] == " ":
-            pula_espaços()    
-        token = next_token(codigo[i])          
+    erros = 0  # Erros de uma linha do código
+    indice = 0  # indice da lista
+    while indice < tamanho:
+        if codigo[indice] == " ":
+            pula_espacos()
+        token = next_token(codigo[indice])
         total_erros += erros
-        analise.append(token)# Adiciona o token
-        if erros > 0:# Pule para próxima linha de código       
-            break       
+        analise.append(token)  # Adiciona o token
+        if erros > 0:  # Pule para próxima linha de código
+            break
     line = arquivo.readline()
-if total_erros == 0:    
+if total_erros == 0:
     print("==================================")
     print("   Analise concluída sem erros  ")
     print("==================================")
     for x in analise:
-        print(">>>",x)
-    print("Total de erros: ",total_erros)   
+        print(">>>", x)
+    print("Total de erros: ", total_erros)
 else:
     print()
-    print("Total de erros: ",total_erros)   
+    print("Total de erros: ", total_erros)
